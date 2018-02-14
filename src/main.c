df3c12aeec860d1d4f5dadfa65cb4259a5b65bc3
Hannes Rabo <hannes.rabo@gmail.com>
2018-02-14 10:51:54 +0100

    Added some project structure and make file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+ * This is the main file for the project
+ *
+ */
+
+#ifndef _REENTRANT
+#define _REENTRANT
+#endif
+
+#include <stdio.h>
+
+int main() {
+	
+	// Everything went well...
+	return 0;
+}
