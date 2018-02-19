fa27767c932f7bb29c2dc218063a0f8fb579ff37
jcelik <julius.cc@hotmail.com>
2018-02-19 15:51:14 +0100

    Added structure

--- src/test-1.c
+++ src/test-1.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h>
 
+#include "../hwAPI/hardwareAPI.h"
+#include "main.h"
+
 int main(int argc, char *argv[])
 {
     /* 1. Start an input thread */
@@ -11,6 +14,8 @@ int main(int argc, char *argv[])
     /* 2. Start an elevator controller for every thread */
 
     /* 3. Start a work distributor */
+
+    /* 4. Do specific stuff */
 
     // Everything went well...
     return 0;
