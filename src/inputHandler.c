6fbaebabacc870bc35bce7a2318435c7bb83eb7a
jcelik <julius.cc@hotmail.com>
2018-02-19 18:30:01 +0100

    Made main main

---
+++ src/inputHandler.c
@@ -0,0 +1,54 @@
+#include "../hwAPI/hardwareAPI.h"
+
+/* TODO: replace all fprint with real code */
+void *inputHandler(void *argument)
+{
+	EventType e;
+	EventDesc ed;
+
+	while (1)
+	{
+		e = waitForEvent(&ed);
+
+		switch (e)
+		{
+		case FloorButton:
+			pthread_mutex_lock(&mutex);
+			fprintf(stdout, "floor button: floor %d, type %d\n",
+					ed.fbp.floor, (int)ed.fbp.type);
+			fflush(stdout);
+			pthread_mutex_unlock(&mutex);
+			break;
+
+		case CabinButton:
+			pthread_mutex_lock(&mutex);
+			fprintf(stdout, "cabin button: cabin %d, floor %d\n",
+					ed.cbp.cabin, ed.cbp.floor);
+			fflush(stdout);
+			pthread_mutex_unlock(&mutex);
+			break;
+
+		case Position:
+			pthread_mutex_lock(&mutex);
+			fprintf(stdout, "cabin position: cabin %d, position %f\n",
+					ed.cp.cabin, ed.cp.position);
+			fflush(stdout);
+			pthread_mutex_unlock(&mutex);
+			break;
+
+		case Speed:
+			pthread_mutex_lock(&mutex);
+			fprintf(stdout, "speed: %f\n", ed.s.speed);
+			fflush(stdout);
+			pthread_mutex_unlock(&mutex);
+			break;
+
+		case Error:
+			pthread_mutex_lock(&mutex);
+			fprintf(stdout, "error: \"%s\"\n", ed.e.str);
+			fflush(stdout);
+			pthread_mutex_unlock(&mutex);
+			break;
+		}
+	}
+}
--- src/main.c
+++ src/main.c
@@ -12,6 +12,21 @@
 int main(int argc, char *argv[])
 {
 
-	// Everything went well...
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s host-name port\n", argv[0]);
+		fflush(stderr);
+		exit(-1);
+	}
+	hostname = argv[1];
+	if ((port = atoi(argv[2])) <= 0)
+	{
+		fprintf(stderr, "Bad port number: %s\n", argv[2]);
+		fflush(stderr);
+		exit(-1);
+	}
+
+	initHW(hostname, port);
+
 	return 0;
 }
