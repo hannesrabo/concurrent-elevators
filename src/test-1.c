022ddd9bb338f5b236bf9ff85c48917db26367b4
jcelik <julius.cc@hotmail.com>
2018-02-19 15:43:09 +0100

    Create outline for test 1

--- src/test-1.c
+++ src/test-1.c
@@ -6,6 +6,11 @@
 
 int main(int argc, char *argv[])
 {
+    /* 1. Start an input thread */
+
+    /* 2. Start an elevator controller for every thread */
+
+    /* 3. Start a work distributor */
 
     // Everything went well...
     return 0;
