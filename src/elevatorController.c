691dea44a6e83541812caa622333032c884cb66f
jcelik <julius.cc@hotmail.com>
2018-03-01 13:17:49 +0100

    Started completing basic elevator controller

--- src/elevatorController.c
+++ src/elevatorController.c
@@ -16,24 +16,41 @@ void *ElevatorController(void *argument)
 	ElevatorInformation *information = (ElevatorInformation *)argument;
 	EventQueueItem *nextEvent;
 
+	double speed = 1;
+	double position = 0;
+
+	getSpeed();
+	whereIs(information->id);
+
 	while (1)
 	{
 		nextEvent = event_queue_front(&information->events);
+
+		switch (nextEvent->type)
+		{
+		case Position:
+			if (nextEvent->event->cp.cabin == information->id)
+				updatePosition(&position, nextEvent->event->cp.position);
+			break;
+		case Speed:
+			speed = nextEvent->event->s.speed;
+			printf("Speed received: %f\n", speed);
+			break;
+		case FloorButton:
+		case CabinButton:
+		case Error:
+			printf("I %d got error!", information->id);
+			break;
+		}
+
 		event_queue_pop(&information->events);
-
-		if (nextEvent->type == Position)
-		{
-			// printf("Error!\n");
-			// printf("Error! %s\n", nextEvent->event->e.str);
-			printf("Position %f received\n", nextEvent->event->cp.position);
-		}
-		else
-		{
-			printf("Next event received!\n");
-		}
-
-		sleep(3);
 	}
 
 	return 0;
+}
+
+void updatePosition(double *currentPosition, double newPosition)
+{
+	*currentPosition = newPosition;
+	printf("Position received: %f\n", *currentPosition);
 }
